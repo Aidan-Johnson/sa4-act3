7f2382c43dd123101d9dbc47ac72bf72e03ac93f
Aidan-Johnson <asjohnsonstu@gmail.com>
2024-02-07 13:48:24 -0600

    Implemented a while loop so guessing game now will run until the user guesses the number or quits by typing q

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+while True:
+    guess = (input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    elif guess == 'q':
+        print(f'The number was {number}!')
+        break
+    else:
+        print(f"Sorry! {guess} was not correct... PLEASE TRY AGAIN!!!1! IF YOU WISH TO EXIT TYPE q")
